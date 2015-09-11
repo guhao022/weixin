962105268478531e18a6f36799e619bd0715b4b5
guhao <378999587@qq.com>
2015-09-11 13:39:50 +0800

    fix

--- jssdk.go
+++ jssdk.go
@@ -79,7 +79,7 @@ func (this *JsapiTicket) Refresh(refresh bool) {
                 if err := this.Get(); err != nil {
                     continue
                 }
-                time.Sleep(time.Second * time.Duration(2 * time.Hour))
+                time.Sleep(time.Second * 7200)
                 //time.Sleep(1 * time.Second)
                 log.Println("Ticket: ", this.fetch())
             }
